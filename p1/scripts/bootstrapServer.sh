8e48c6cbc911f9239ccdb6ca7b325791b1abc27a
eprei <93380002+eprei@users.noreply.github.com>
2024-08-17 16:07:37 +0200

    feat: vagrantfile added in p1

---
+++ p1/scripts/bootstrapServer.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "bootstrapping server..."
+#This line is just to verify that the shell script is running correctly in the virtual machine.
+touch bootstrapServer.txt
+#TODO Install K3s and configure it
---
+++ p1/scripts/bootstrapServerWorker.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "bootstrapping serverWorker..."
+#This line is just to verify that the shell script is running correctly in the virtual machine.
+touch bootstrapServerWorker.txt
+#TODO Install K3s and configure it
