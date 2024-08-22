ef62641873a22abd61d59a1ea91c7da60842e713
Robin Burri <robinburri@tutanota.com>
2024-08-23 01:32:42 +0200

    add server flag with ip:port

--- p1/scripts/bootstrapServerWorker.sh
+++ p1/scripts/bootstrapServerWorker.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 echo "Installing K3s..."
-wget -q -O - https://get.k3s.io | sh -
+wget -q -O - https://get.k3s.io | \
+	INSTALL_K3S_EXEC="agent" \
+	K3S_TOKEN="mypassword" \
+	sh -s - \
+	--flannel-iface 'eth1' \
+	--server "https://192.168.56.110:6443"
+
 if [ $? == 0 ] ; then
    echo "K3s successfully installed"
 else
