a74041e238461b2a570c0b28bfe50bf78f0950bb
eprei <93380002+eprei@users.noreply.github.com>
2024-09-05 09:52:09 +0200

    clean comments script p2 (#33)

--- p2/scripts/bootstrapServer.sh
+++ p2/scripts/bootstrapServer.sh
@@ -26,7 +26,6 @@ kubectl apply -f $clusterConfigsFolder/app-three.yml
 
 echo "Installing NGINX Ingress Controller"
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.11.2/deploy/static/provider/cloud/deploy.yaml
-# doc https://kubernetes.github.io/ingress-nginx/deploy/#quick-start
 
 echo "Disabling the validation webhook..."
 kubectl apply -f $clusterConfigsFolder/ingress-nginx-no-validation.yml
@@ -49,21 +48,3 @@ while ! kubectl get ingress | grep -q "192.168.56.110" ; do
     sleep 10
 done
 echo "Ingres Controller configured and ready to receive requests."
-
-# Useful commands to check the status of the “ingress controller”
-
-# Check the status of the pods to see if the ingress controller is online.
-#   kubectl get pods --namespace ingress-nginx
-
-# Check to see if the service is online. This of type NodePort, so do you have an EXTERNAL-IP?
-#   kubectl get services --namespace ingress-nginx
-
-# Check out the ingressclass nginx.
-# Ingresses we will need specify an ingressclassname
-#   kubectl describe ingressclasses nginx
-
-# Get the status of the ingress.
-#   kubectl get ingress --watch # Wait for the Address of intress-path to be populated before proceeding
-#   kubectl get services --namespace ingress-nginx
-
-#   kubectl describe ingress
